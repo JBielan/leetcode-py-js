46de68b68c40de4ec35ceb7a0f5e595122e34a09
Jakub Bielan <31440616+jbielan@users.noreply.github.com>
2019-04-05 11:01:28 +0200

    remove_element - solved (correction)

---
+++ binary_search.py
@@ -0,0 +1,22 @@
+class Solution:
+    def searchInsert(self, nums, target):
+        left, right = 0,len(nums)-1     # At the beginning whole range is taken into account (0, len-1)
+        while left<right:               # As long as bottom frontier touches upper, constantly narrow range
+            mid = (left+right)//2       # Always check the middle of the range
+            if nums[mid]<target:        # If middle is smaller, you cut bottom by setting bottom to middle
+                left = mid+1            # Left forntier is set to the middle
+            else:
+                right = mid             # If it's bigger or equal, cut upper half by setting upper to middle
+
+        # Outer while, which means bottom frontier touched upper (index has been found)
+
+        if nums[left]<target:           # It means algorithm touched the end of the list, so add target as last item
+            return left+1
+        return left                     # Otherwise index is the bottom frontier
+
+"""
+I hope I helped you in some way. If you like this solution, I'll be very happy if you click "Star" on Github:
+https://github.com/JBielan/leetcode-python
+
+May the code be with you!
+"""
