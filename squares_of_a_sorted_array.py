9d18f6056841a66c6009c32f0563c0086b93c909
Jakub Bielan <31440616+jbielan@users.noreply.github.com>
2019-04-15 13:09:58 +0200

    flipping_an_image and 3 other challenges solved

--- squares_of_a_sorted_array.py
+++ squares_of_a_sorted_array.py
@@ -3,6 +3,7 @@ class Solution:
 		return sorted([number**2 for number in A])  # list comprehension creates a list of squares and sorted sorts it
 
 """
+
 I hope I helped you in some way. If you like this solution, I'll be very happy if you click "Star" on Github:
 https://github.com/JBielan/leetcode-python
 
