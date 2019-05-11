e3793ec3c89a49f2f8368c6d74eb5e8e03c23817
Jakub Bielan <31440616+jbielan@users.noreply.github.com>
2019-05-11 12:48:43 +0200

    reverse_words - solved by 1-liner

--- reverse_words_1_line.py
+++ reverse_words_1_line.py
@@ -2,3 +2,4 @@ class Solution:
 	def reverseWords(self, s: str) -> str:
 		return ' '.join([word[::-1] for word in s.split(' ')])      # s.split(' ') creates a list of words,
         # list comprehension diverts symbols within a word and ' '.join() joins them back into a string
+
