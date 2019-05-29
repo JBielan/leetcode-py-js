1a815ab153aff32ba1d83c7c22aa64e1a41b98e9
JBielan <jkbielan@gmail.com>
2019-05-29 15:21:23 +0200

    uncommon_words - solved!

---
+++ uncommon_words.py
@@ -0,0 +1,4 @@
+class Solution:
+	def uncommonFromSentences(self, A: str, B: str):
+		both = A.split(' ') + B.split(' ')      # create a list of words
+		return [x for x in both if both.count(x)==1]        # standard list comprehension
