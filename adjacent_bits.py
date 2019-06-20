ddf0f4b1ed26c9131a995c57541b739776f8bbe7
Jakub Bielan <31440616+jbielan@users.noreply.github.com>
2019-06-20 11:45:35 +0200

    adjacent_bits - solved

--- adjacent_bits.py
+++ adjacent_bits.py
@@ -4,4 +4,5 @@ class Solution:
 		for i in range(2, len(binary ) - 1):        # starts from second idx and end at next-to-last
 			if binary[i] == binary[i + 1]:      # if adjacent bits are the same
 				return False        # return False
+
 		return True     # outside of the loop if False wasn't returned, return True
