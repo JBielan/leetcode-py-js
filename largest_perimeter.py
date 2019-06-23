ab4f5e4b125b76c623b8eaa0dc57452e316ce412
Jakub Bielan <31440616+jbielan@users.noreply.github.com>
2019-06-23 19:01:19 +0200

    largest_perimiter - solved

--- largest_perimeter.py
+++ largest_perimeter.py
@@ -4,4 +4,5 @@ class Solution:
 		for i in range(len(A) - 1, 1, -1):      # from last element till index 2 to always get 3 sides
 			if A[i-2] + A[i-1] > A[i]:      # if 2 smaller sides are bigger than the biggest
 				return A[i-2] + A[i-1] + A[i]       # return the sum
+
 		return 0
